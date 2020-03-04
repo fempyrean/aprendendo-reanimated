4e453a813787d5aedc19954a54956ae1127ea36c
Lucas Sousa <frempyrean@gmail.com>
2020-03-03 21:24:47 -0300

    Linkando reanimated e movendo App.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable import/no-named-as-default-member */
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
