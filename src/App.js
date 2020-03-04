8a34b2fe22c7043ed52f97797e0003f27c000430
Lucas Sousa <frempyrean@gmail.com>
2020-03-04 20:29:11 -0300

    Rotation e WidhtHeight

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 // import Progress from './components/Progress';
 // import Sequence from './components/Sequence';
 // import Shuffle from './components/Shuffle';
-import Ticket from './components/Ticket';
+// import Ticket from './components/Ticket';
+// import ImageViewer from './components/ImageViewer';
+// import Rotation from './components/Rotation';
+import WidthHeight from './components/WidthHeight';
 
 export default function App() {
-    return <Ticket />;
+    return <WidthHeight />;
 }
